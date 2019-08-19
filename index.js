8bbf5d6daa494bd00e3b5aaa124687e5d11f13c3
MarvinKweyu <mkweyu1@gmail.com>
2019-08-19 08:07:53 +0300

    Use react_vr to create image album

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
 
-import Navigation from "./components/nav/Navigation"
-import InfoPopup from "./components/popup/InfoPopup"
-import Card from "./components/card/Card"
+// import Navigation from "./components/nav/Navigation"
+// import InfoPopup from "./components/popup/InfoPopup"
+// import Card from "./components/card/Card"
 
-export {
-  Navigation,
-  InfoPopup,
-  Card,
-};
+// export {
+//   Navigation,
+//   InfoPopup,
+//   Card,
+// };
 
 import {
   AppRegistry,
+  StyleSheet,
+  Text,
+  View,
   VrButton,
 } from 'react-360';
 
-export default class home_expo extends Component {
-  constructor() {
+import Canvas from './components/Canvas';
+
+
+export default class home_expo extends React.Component {
+
+  constructor(){
     super();
+
     this.state = {
-  
-      };
+      src: './static_assets/bedroom_mirror.jpeg',
+    };
   }
+
+
   render() {
     return (
-      //<VrButton>Click me!</VrButton>
-    );
-  }
-};
+
+      <View >
+      <Canvas
+         src={this.state.src}
+      />
+      
+        </View>
+        );
+      }
+    };
+
 
 AppRegistry.registerComponent('home_expo', () => home_expo);
